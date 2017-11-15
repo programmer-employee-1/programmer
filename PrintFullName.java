7b93e6ab88ce3267ac95f5318b373f5a1364aa80
rene.catapang <rene.catapang@gmail.com>
2017-11-15 10:36:01 +0800

    added print to rene branch

--- PrintFullName.java
+++ PrintFullName.java
@@ -3,5 +3,6 @@ public class PrintFullName {
         System.out.println("John Smith");
         System.out.println("Rene Catapang");
         System.out.println("print from first branch");
+        System.out.println("print from rene branch");
     }
 }
